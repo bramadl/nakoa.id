0b3152b60b301afcc1cf54ced53a2ce7e2b6f668
Bram Adl <thenamesbram@gmail.com>
2022-08-15 04:18:22 +0700

    fix: move gallery section to layouts

--- pages/index.vue
+++ pages/index.vue
@@ -94,25 +94,7 @@ const bestSellerMenus = ref([
   },
 ]);
 
-const cafeDetails = ref([
-  {
-    name: "Coffee",
-    description: "All our coffee selections in hot, iced, and frappe",
-    amount: 18,
-  },
-  {
-    name: "Non-coffee",
-    description: "Our non-coffee beverages in hot, iced, and frappe",
-    amount: 25,
-  },
-  {
-    name: "Snack",
-    description: "Mouthwatering sweet & savory snack selections",
-    amount: 20,
-  },
-]);
-
-const instagramPictures = useInstagramPictures();
+const cafeDetails = useCafeDetails();
 
 const onSwiper = (swiper: any) => {
   swiperInstance.value = swiper;
@@ -403,32 +385,5 @@ const onSlideNext = () => {
       </div>
     </section>
     <!-- ./Cafe Detail Section -->
-
-    <!-- Gallery Section -->
-    <section class="relative w-full bg-white text-slate-900">
-      <div
-        class="container mx-auto flex flex-col gap-8 px-4 md:px-8 lg:px-12 py-8 lg:py-16"
-      >
-        <BaseHeader>
-          <template #title>
-            <BaseHeading text="Instagram Kami" />
-          </template>
-          <template #action>
-            <a href="#" class="text-brand">@nakoacafe</a>
-          </template>
-        </BaseHeader>
-
-        <div
-          class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 md:gap-8"
-        >
-          <img
-            v-for="img in instagramPictures"
-            :src="img"
-            alt="Instagram Picture"
-          />
-        </div>
-      </div>
-    </section>
-    <!-- ./Gallery Section -->
   </div>
 </template>
