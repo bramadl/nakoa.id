9cf4c3c95c0c3772bd45fd54c61c8c030b2e67c5
Bram Adl <thenamesbram@gmail.com>
2022-08-15 03:32:23 +0700

    chore: create instagram pictures global state

--- pages/index.vue
+++ pages/index.vue
@@ -112,14 +112,7 @@ const cafeDetails = ref([
   },
 ]);
 
-const instagramPictures = ref([
-  "https://images.squarespace-cdn.com/content/v1/60be0295968e974e71404829/1624359235772-2W6OJCYTUD8RO9MU4L8E/image-asset.jpeg?format=500w",
-  "https://images.squarespace-cdn.com/content/v1/60be0295968e974e71404829/1624359236681-2472G2MEENVRUW4AH169/image-asset.jpeg?format=500w",
-  "https://images.squarespace-cdn.com/content/v1/60be0295968e974e71404829/1624359237196-HBF79NBBJYKLHI2XUGJG/image-asset.jpeg?format=500w",
-  "https://images.squarespace-cdn.com/content/v1/60be0295968e974e71404829/1624359235772-2W6OJCYTUD8RO9MU4L8E/image-asset.jpeg?format=500w",
-  "https://images.squarespace-cdn.com/content/v1/60be0295968e974e71404829/1624359236681-2472G2MEENVRUW4AH169/image-asset.jpeg?format=500w",
-  "https://images.squarespace-cdn.com/content/v1/60be0295968e974e71404829/1624359237196-HBF79NBBJYKLHI2XUGJG/image-asset.jpeg?format=500w",
-]);
+const instagramPictures = useInstagramPictures();
 
 const onSwiper = (swiper: any) => {
   swiperInstance.value = swiper;
