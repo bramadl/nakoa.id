fe70bcb03ecb48d2a70f2aedc809b100e00824fa
Bram Adl <thenamesbram@gmail.com>
2022-08-14 22:53:09 +0700

    chore: display demo page on other pages

---
+++ pages/menus.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="h-screen flex items-center justify-center">
+    <h2 class="uppercase font-bold text-2xl md:text-4xl lg:text-8xl">
+      This site is only a demo
+    </h2>
+  </section>
+</template>
